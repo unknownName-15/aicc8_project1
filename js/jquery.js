042b9a25242877a85ce1833e8ac132d7c4f90620
unknownName-15 <imsicode.15@gmail.com>
2026-01-20 16:15:04 +0900

    완성본

---
+++ js/jquery.js
@@ -0,0 +1,14 @@
+// $(document).ready(function() {
+//   // some jquery codes
+// });
+
+$(function () {
+  $('.nav-lists li').on('click', function () {
+    const targetIndex = $(this).index(); // 클릭한 대상의 인덱스 저장
+    const pagePosition = $('.nav-target').eq(targetIndex).offset().top; // 해당 인덱스의 .nav-target 위치 지정
+
+    $('html, body').animate({ scrollTop: pagePosition }, 300);
+  });
+
+  console.log($('.nav-target'));
+});
